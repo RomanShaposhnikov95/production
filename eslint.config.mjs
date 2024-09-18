6f6a5b70c8f366582e7d193f9cdf0037e312ddb8
RomanShaposhnikov95 <romanshaposhnikov95@gmail.com>
2024-09-18 23:17:31 +0300

    add stylelint, i18n, plugin for eslint, fix all styles

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,7 @@ import globals from 'globals';
 import pluginJs from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import pluginReact from 'eslint-plugin-react';
+import i18next from 'eslint-plugin-i18next';
 
 
 export default [
@@ -19,6 +20,7 @@ export default [
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  i18next.configs['flat/recommended'],
   {
     rules: {
       'space-before-function-paren': ['error', 'always'], // Требовать пробел перед функцией
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
 import cls from './Navbar.module.scss';
 import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
+import { useTranslation } from 'react-i18next';
 
 interface NavbarProps {
     className?: string;
 }
 
 export const Navbar = ({ className }: NavbarProps) => {
+  const { t } = useTranslation();
   return (
     <div className={classNames(cls.navbar, {}, [className])}>
       <div className={cls.links}>
@@ -15,13 +17,13 @@ export const Navbar = ({ className }: NavbarProps) => {
           theme={AppLinkTheme.SECONDARY}
           to={'/'}
           className={cls.mainLink}>
-                    Главная
+          {t('Пипка')}
         </AppLink>
         <AppLink
           theme={AppLinkTheme.SECONDARY}
           to={'/about'}
         >
-                    О сайте
+          {t('О сайте')}
         </AppLink>
       </div>
     </div>
