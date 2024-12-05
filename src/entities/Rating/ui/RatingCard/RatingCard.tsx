ef8f14b8cf3350f3befa73a272afd3911a710e03
RomanShaposhnikov95 <romanshaposhnikov95@gmail.com>
2024-12-05 13:27:30 +0200

    add article rating card

--- src/entities/Rating/ui/RatingCard/RatingCard.tsx
+++ src/entities/Rating/ui/RatingCard/RatingCard.tsx
@@ -18,8 +18,10 @@ interface RatingCardProps {
     hasFeedback?: boolean;
     onCancel?: (starsCount: number) => void;
     onAccept?: (starsCount: number, feedback?: string) => void;
+    rate?: number;
 }
 
+// eslint-disable-next-line react/display-name
 export const RatingCard = memo((props: RatingCardProps) => {
   const {
     className,
@@ -28,14 +30,14 @@ export const RatingCard = memo((props: RatingCardProps) => {
     hasFeedback,
     onCancel,
     title,
+    rate = 0,
   } = props;
   const { t } = useTranslation();
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [starsCount, setStarsCount] = useState(0);
+  const [starsCount, setStarsCount] = useState(rate);
   const [feedback, setFeedback] = useState('');
 
   const onSelectStars = useCallback((selectedStarsCount: number) => {
-    console.log('click', selectedStarsCount)
     setStarsCount(selectedStarsCount);
     if (hasFeedback) {
       setIsModalOpen(true);
@@ -68,10 +70,10 @@ export const RatingCard = memo((props: RatingCardProps) => {
   );
 
   return (
-    <Card className={classNames('', {}, [className])}>
-      <VStack align="center" gap="8">
-        <Text title={title} />
-        <StarRating size={40} onSelect={onSelectStars} />
+    <Card className={className} max>
+      <VStack align="center" gap="8" max>
+        <Text title={starsCount ? t('Спасибо за оценку!') : title} />
+        <StarRating selectedStars={starsCount} size={40} onSelect={onSelectStars} />
       </VStack>
       <BrowserView>
         <Modal isOpen={isModalOpen} lazy>
--- src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
+++ src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
@@ -1,40 +1,17 @@
 import { classNames } from '@/shared/lib/classNames/classNames';
 import { useTranslation } from 'react-i18next';
-import { memo, useCallback } from 'react';
-import { ArticleDetails, ArticleList } from '@/entities/Article';
+import { memo } from 'react';
+import { ArticleDetails } from '@/entities/Article';
 import cls from './ArticleDetailsPage.module.scss';
-import { useNavigate, useParams } from 'react-router-dom';
-import { Text, TextSize } from '@/shared/ui/Text/Text';
-import { CommentList } from '@/entities/Comment';
+import { useParams } from 'react-router-dom';
 import { DynamicModuleLoader, ReducersList } from '@/shared/lib/components/DynamicModuleLoader/DynamicModuleLoader';
-import { articleDetailsCommentsReducer, getArticleComments } from '../../model/slices/articleDetailsCommentsSlice';
-import { useDispatch, useSelector } from 'react-redux';
-import { RoutePath } from '@/shared/config/routeConfig/routeConfig';
-import { getArticleCommentsIsLoading } from '../../model/selectors/comments';
-import { useInitialEffect } from '@/shared/lib/hooks/useInitialEffect/useInitialEffect';
-import {
-  fetchCommentsByArticleId,
-} from '../../model/services/fetchCommentsByArticleId/fetchCommentsByArticleId';
-import { useAppDispatch } from '@/shared/lib/hooks/useAppDispatch/useAppDispatch';
-import { AddCommentForm } from '@/features/addCommentForm';
-import {
-  addCommentForArticle,
-} from '../../model/services/addCommentForArticle/addCommentForArticle';
-import { Button, ThemeButton } from '@/shared/ui/Button/Button';
 import { Page } from '@/widgets/Page/Page';
-import {
-  articleDetailsPageRecommendationsReducer,
-  getArticleRecommendations,
-} from '../../model/slices/articleDetailsPageRecommendationsSlice';
-import { getArticleRecommendationsIsLoading } from '../../model/selectors/recommendations';
 import { articleDetailsPageReducer } from '../../model/slices';
-import {
-  fetchArticleRecommendations,
-} from '../../model/services/fetchArticleRecommendations/fetchArticleRecommendations';
 import { ArticleDetailsPageHeader } from '../ArticleDetailsPageHeader/ArticleDetailsPageHeader';
 import { VStack } from '@/shared/ui/Stack';
 import { ArticleRecommendationsList } from '@/features/articleRecommendationsList';
 import { ArticleDetailsComments } from '../ArticleDetailsComments/ArticleDetailsComments';
+import { ArticleRatingAsync } from '@/features/articleRating/ui/ArticleRating/ArticleRating.async';
 
 
 interface ArticleDetailsPageProps {
@@ -51,6 +28,10 @@ const ArticleDetailsPage = (props: ArticleDetailsPageProps) => {
   const { t } = useTranslation();
   const { id } = useParams<{ id: string }>();
 
+  if (!id) {
+    return null;
+  }
+
 
   return (
     <DynamicModuleLoader reducers={reducers} removeAfterUnmount>
@@ -58,6 +39,7 @@ const ArticleDetailsPage = (props: ArticleDetailsPageProps) => {
         <VStack gap={'16'} max>
           <ArticleDetailsPageHeader />
           <ArticleDetails id={id} />
+          <ArticleRatingAsync articleId={id} />
           <ArticleRecommendationsList />
           <ArticleDetailsComments id={id} />
         </VStack>
